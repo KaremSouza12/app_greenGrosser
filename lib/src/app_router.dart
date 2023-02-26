5884129fa463655b772c1351064bc193fbe37be9
KaremSouza12 <fulldevelop340@gmail.com>
2023-02-25 22:38:02 -0300

    product_screen and quantity_widget

--- lib/src/app_router.dart
+++ lib/src/app_router.dart
@@ -1,11 +1,14 @@
+import 'package:app_greengrosser/src/models/item_models.dart';
 import 'package:app_greengrosser/src/pages/auth/sigin_up_screen.dart';
 import 'package:app_greengrosser/src/pages/auth/signin_screen.dart';
 import 'package:app_greengrosser/src/pages/base/base_screen.dart';
+import 'package:app_greengrosser/src/pages/product/product_screen.dart';
 import 'package:go_router/go_router.dart';
 
 const initialRoute = '/';
 const signinUpRoute = '/signin_up';
 const baseScreenRoute = '/base_screen';
+const productScreenRoute = '/product_screen';
 
 final routes = GoRouter(routes: [
   GoRoute(
@@ -19,5 +22,14 @@ final routes = GoRouter(routes: [
   GoRoute(
     path: baseScreenRoute,
     builder: (_, __) => const BaseScreen(),
+  ),
+  GoRoute(
+    path: productScreenRoute,
+    builder: (_, state) {
+      final params = state.extra! as ItemModel;
+      return ProductScreen(
+        item: params,
+      );
+    },
   ),
 ]);
--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,7 +1,9 @@
+import 'package:app_greengrosser/src/app_router.dart';
 import 'package:app_greengrosser/src/config/custom_colors.dart';
 import 'package:app_greengrosser/src/models/item_models.dart';
 import 'package:app_greengrosser/src/services/utils_service.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ItemTile extends StatelessWidget {
   ItemTile({super.key, required this.item});
@@ -11,52 +13,83 @@ class ItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            //Image
-            Expanded(
-              child: Image.asset(item.imgUrl),
+    return Stack(
+      children: [
+        GestureDetector(
+          onTap: () => context.push(productScreenRoute, extra: item),
+          child: Card(
+            elevation: 1,
+            shadowColor: Colors.grey.shade300,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
-            Text(
-              item.itemName,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  //Imagem
+                  Expanded(
+                    child: Hero(
+                      tag: item.imgUrl,
+                      child: Image.asset(item.imgUrl),
+                    ),
+                  ),
+                  Text(
+                    item.itemName,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  //Price
+                  Row(
+                    children: [
+                      Text(
+                        utilsServices.priceToCurrency(item.price),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: CustomColors.customSwatchColor,
+                        ),
+                      ),
+                      Text(
+                        '/${item.unit}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Colors.grey.shade500,
+                        ),
+                      )
+                    ],
+                  )
+                ],
               ),
             ),
-            //Price
-            Row(
-              children: [
-                Text(
-                  utilsServices.priceToCurrency(item.price),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: CustomColors.customSwatchColor,
-                  ),
-                ),
-                Text(
-                  '/${item.unit}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12,
-                    color: Colors.grey.shade500,
-                  ),
-                )
-              ],
-            )
-          ],
+          ),
         ),
-      ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                  color: CustomColors.customSwatchColor,
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(15),
+                      topRight: Radius.circular(20))),
+              child: const Icon(
+                Icons.shopping_cart_checkout_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
